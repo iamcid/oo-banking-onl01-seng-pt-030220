9f6c5926fa2897a1ae515b4aceb7f6e8cf704cd9
Christopher Cid <iamcid17@gmail.com>
2020-04-03 01:49:04 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,11 +3,10 @@ class BankAccount
   attr_accessor :balance, :open
   attr_reader :name
 
-  def initialize(name, balance = 1000, status = "open")
+  def initialize(name, balance = 1000, status = 'open')
     @name = name
     @balance = balance
     @status = status
-    
   end
 
 end
