640af3d242dfcd6aaa9dc69e27231b00fa0fffe6
Christopher Cid <iamcid17@gmail.com>
2020-04-03 02:06:08 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -17,12 +17,7 @@ class BankAccount
     "Your balance is $#{@balance}."
   end
   
-  def valid?(status)
-    if @status == "open" && @balance > 0
-      true
-    else
-      false
-    end
+  def valid?(status, balance)
   end
   
   def close_account
