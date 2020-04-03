bb013217370b16d841bda4e6dd552a1c9071849e
Christopher Cid <iamcid17@gmail.com>
2020-04-03 02:08:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
   
   def initialize(sender, receiver, status = "pending", amount)
     @sender, @receiver, @status, @amount = sender, receiver, status, amount
-    @amount = 
+  end
   
   
   
